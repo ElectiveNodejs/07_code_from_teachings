ac2acfeab0a8d05cc8a9eceac8e4b0ac4b9c89ef
clbokea <clbo@kea.dk>
2017-11-03 13:35:32 +0100

    sjdhfks

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+app.use(function(req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+
 const customersModule = require('./routes/customers.js');
 app.use(customersModule);
 
